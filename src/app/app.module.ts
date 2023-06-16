0277023c8dc4c80f6b928641850270b901a735a2
Kostas Christou <christou@exeo.gr>
2023-06-16 16:26:06 +0300

    added lazy loaded modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewsModule } from './views/views.module';
 import { ModsModule } from './mods/mods.module';
-import { ElementsModule } from './elements/elements.module';
-import { CollectionsModule } from './collections/collections.module';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [
-    BrowserModule,
-    ElementsModule,
-    CollectionsModule,
-    ViewsModule,
-    ModsModule,
-    AppRoutingModule,
-  ],
+  imports: [BrowserModule, ViewsModule, ModsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
